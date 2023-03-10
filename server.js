046a1dea30f72f5b2e2adbb1cbc5a4f4317a3cb0
Ariel D'Alessandro <ariel.dalessandro@gmail.com>
2023-03-10 13:17:46 -0300

    server: Conditionally select gato/perro template
    Signed-off-by: Ariel D'Alessandro <ariel.dalessandro@gmail.com>

--- server.js
+++ server.js
@@ -5,24 +5,31 @@ var XlsxTemplate = require('xlsx-template');
 
 var app = hl7.tcp();
 
-const templateFilename = 'HEMOGRAMA FELINO.xlsx';
-
 function strToFloat(s) {
 	return parseFloat(s.replace(',', '.'));
 }
 
 function generateExcel(filename, values) {
+	values['Resultados'].forEach(resultado => {
+		values[resultado['Parametro']] = strToFloat(resultado['Resultado']);
+	});
+	values['RBC'] *= 1000000;
+	values['PLT'] *= 1000;
+	values['WBC'] *= 1000;
+	const especie = values['Especie'];
+	var templateFilename;
+	if (especie == 'Gato') {
+		templateFilename = 'HEMOGRAMA FELINO.xlsx';
+	} else if (especie == 'Perro') {
+		templateFilename = 'HEMOGRAMA CANINO.xlsx';
+	} else {
+		console.error("Especie desconocida " + especie + ", omitiendo...");
+		return;
+	}
 	fs.readFile(path.join(__dirname, templateFilename), function(err, data) {
 		const template = new XlsxTemplate(data);
 		const sheetNumber = 1;
-		var outputValues = {};
-		values['Resultados'].forEach(resultado => {
-			outputValues[resultado['Parametro']] = strToFloat(resultado['Resultado']);
-		});
-		outputValues['RBC'] *= 1000000;
-		outputValues['PLT'] *= 1000;
-		outputValues['WBC'] *= 1000;
-		template.substitute(sheetNumber, outputValues);
+		template.substitute(sheetNumber, values);
 		const outputData = template.generate();
 		const outputFilename = filename + '.xlsx';
 		fs.writeFileSync(path.join(__dirname, outputFilename), outputData, 'binary');
